90a16e5e8662f6d7fb4e05415ce71fd08334d5d9
github_username <you@domain.com>
2019-12-04 11:20:23 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>hello world</h1>
+  </div>
+</template>
+
+
+<script>
+
+  export default {
+    name: 'app',
+
+    components: {
+      
+    }
+  }
+</script>
+
+<style>
+  @import "./assets/css/base.css";
+</style>
